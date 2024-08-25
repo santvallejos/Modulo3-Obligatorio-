3319d02caf06ca6a61a041bc44fcf7378d048d27
Santiago Vallejos <vallejossantiago1412@gmail.com>
2024-08-25 20:12:54 -0300

    Modificacion en User (Al recibir los datos)

--- Program.cs
+++ Program.cs
@@ -1,13 +1,2 @@
 ﻿using Modulo3_Obligatorio;
 
-//Añadir y mostrar los registros de temperatura
-WeatherForecast3 RegistrosClima = new WeatherForecast3();
-
-User usuario1 = new User("Santiago", "Pasante");
-
-TemperatureRecord registro1 = new TemperatureRecord(27, usuario1, "21-08-24", "16:40");
-
-RegistrosClima.AddTemperatureRecord(registro1, 0,0);
-
-RegistrosClima.DisplayAllTemperatures();
-
